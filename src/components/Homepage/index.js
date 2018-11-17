3a7c65ca25ea6082ed359390f3195d8a9380a778
Nam Doan Ngoc <zinzinhust96@gmail.com>
2018-11-17 08:49:51 +0700

    initial commit

---
+++ src/components/Homepage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Homepage extends React.PureComponent {
+  render() {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+}
+
+export default Homepage
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Homepage from './components/Homepage'
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div id="page">
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Homepage
+                  {...props}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
